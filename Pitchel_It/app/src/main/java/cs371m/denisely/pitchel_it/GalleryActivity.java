0f11ec0f00a1bb1496a352955efca4fd9b05925c
Denise Ly <dply95@gmail.com>
2016-11-06 19:40:05 -0600

    Replace splash screen

---
+++ Pitchel_It/app/src/main/java/cs371m/denisely/pitchel_it/GalleryActivity.java
@@ -0,0 +1,17 @@
+package cs371m.denisely.pitchel_it;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Denise on 11/6/2016.
+ */
+
+public class GalleryActivity extends Activity {
+    public void onCreate(Bundle savedInstancestate){
+        super.onCreate(savedInstancestate);
+        Intent intent = getIntent();
+        
+    }
+}
--- Pitchel_It/app/src/main/java/cs371m/denisely/pitchel_it/MainActivity.java
+++ Pitchel_It/app/src/main/java/cs371m/denisely/pitchel_it/MainActivity.java
@@ -5,14 +5,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
+
 
 import java.io.File;
 
@@ -32,26 +30,49 @@ public class MainActivity extends Activity implements PermissionRequest.Response
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
-        SettingsList settingsList = new SettingsList();
+        final SettingsList settingsList = new SettingsList();
         settingsList
                 // Set custom camera export settings
                 .getSettingsModel(CameraSettings.class)
-                .setExportDir(Directory.DCIM, "Test")
+                .setExportDir(Directory.DCIM, "Pitchel-It")
                 .setExportPrefix("camera_")
                 // Set custom editor export settings
                 .getSettingsModel(EditorSaveSettings.class)
-                .setExportDir(Directory.DCIM, "Test")
+                .setExportDir(Directory.DCIM, "Pitchel-It")
                 .setExportPrefix("result_")
                 .setSavePolicy(
                         EditorSaveSettings.SavePolicy.RETURN_ALWAYS_ONLY_OUTPUT
                 );
 
-        new CameraPreviewBuilder(this)
-                .setSettingsList(settingsList)
-                .startActivityForResult(this, CAMERA_PREVIEW_RESULT);
+        Button editPhoto = (Button) findViewById(R.id.editButton);
+        editPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new CameraPreviewBuilder(MainActivity.this)
+                        .setSettingsList(settingsList)
+                        .startActivityForResult(MainActivity.this, CAMERA_PREVIEW_RESULT);
+            }
+        });
+
+        Button importPhoto = (Button) findViewById(R.id.importPhotoButton);
+        importPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button galleryButton = (Button) findViewById(R.id.galleryButton);
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
     @Override
